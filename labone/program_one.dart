75a537de137311b5b66b367af5a5027187f8c310
Munyeshya <obedbenny54@gmail.com>
2024-07-02 19:44:49 +0200

    Write cleaner code

--- labone/program_one.dart
+++ labone/program_one.dart
@@ -1,17 +1,24 @@
 import 'dart:io';
 
 void main() {
-  var fname = "Munyeshya";
-  var lname = "Beni Obed";
-  print("My full name is $fname $lname");
-  print('Enter the first number:');
+  print("Enter Your first name:");
+  String? fname = stdin.readLineSync();
+  print("Enter Your last name:");
+  String? lname = stdin.readLineSync();
+
+  print("Enter the Age of a person:");
+  int? age = int.parse(stdin.readLineSync()!);
+
+  if (age >= 18) {
+    print("Your full name is $fname $lname and You are voter.");
+  } else {
+    print("Your full name is $fname $lname and You are not voter.");
+  }
 
   print("Enter the first number:");
   int? num1 = int.parse(stdin.readLineSync()!); //declaring number1
   print("Enter the second number:");
   int? num2 = int.parse(stdin.readLineSync()!);
-  print("Enter the Age of a person:");
-  int? age = int.parse(stdin.readLineSync()!);
 
   int sum = num1 + num2;
   int diff = num1 - num2;
@@ -21,12 +28,6 @@ void main() {
   int div2 = num1 ~/ num2; // integer division
   int mod = num1 % num2;
 
-  if (age >= 18) {
-    print("You are voter.");
-  } else {
-    print("You are not voter.");
-  }
-
   print("The sum is $sum");
   print("The diff is $diff");
   print("The mul is $mul");
@@ -34,7 +35,8 @@ void main() {
   print("The integer division is $div2.");
   print("The modulus is $mod.");
 
-  for (int i = 1; i <= 10; i++) {
-    print(i);
+  stdout.write("Numbers from $num1 to $num2 are: ");
+  for (int i = num1; i <= num2; i++) {
+    stdout.write(" $i");
   }
 }
